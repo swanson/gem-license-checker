a8f9e64999ff758364cc0ebfae6abcf7cde75ad6
Matt Swanson <mdswanson@sep.com>
2013-03-03 15:36:20 -0500

    remove rails from unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,11 +8,9 @@ before_fork do |server, worker|
   # Replace with MongoDB or whatever
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
-    Rails.logger.info('Disconnected from ActiveRecord')
   end
 
   @delayed_job_pid ||= spawn("bundle exec rake jobs:work")
-  Rails.logger.info("Spawning delayed_job worker: #{@delayed_job_pid}")
 
   sleep 1
 end
@@ -21,6 +19,5 @@ after_fork do |server, worker|
   # Replace with MongoDB or whatever
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
-    Rails.logger.info('Connected to ActiveRecord')
   end
 end
